991e895063d5c25e7911f42421c0e0d1a2a07a39
cereceres <jesus.edelcereceres@gmail.com>
2017-06-28 16:33:50 -0500

    the test are ready for every method

--- index.js
+++ index.js
@@ -9,17 +9,21 @@ module.exports = class extends Core {
         const path = '/project';
         return this.api(path, 'POST', data)
           .then((response) => {
-              this._project = response.resource;
+              this._project = response;
               return response;
           });
     }
 
-    getProject() {
-        const path = this._project;
+    getProject(qs) {
+        const path = this._project.resource;
 
         if (!this._project) return this.getAllProjects();
 
-        return this.api(path, 'GET');
+        return this.api(path, 'GET', {}, {}, qs);
+    }
+    setProject(project) {
+        this._project = project;
+        return this;
     }
 
     getAllProjects() {
@@ -38,7 +42,83 @@ module.exports = class extends Core {
     updateProject(data) {
         if(!this._project) return Promise.resolve();
 
-        const path = this._project;
-        return this.api(path, 'PUT', data);
+        const path = this._project.resource;
+        return this.api(path, 'PUT', data)
+            .then((response) => {
+                this._project = response;
+                return response;
+            });
+    }
+
+    createSourceFromUrl(url, options = {}) {
+        const data = { remote: url };
+        Object.assign(data, options);
+        data.project = data.project || this._project;
+        const path = '/source';
+        return this.api(path, 'POST', data)
+            .then((response) => {
+                this._source = response;
+                return response;
+            });
+    }
+
+    createSource(_data, options = {}) {
+        const data = { data: _data.toString() };
+        Object.assign(data, options);
+        data.project = data.project || this._project;
+        const path = '/source';
+        return this.api(path, 'POST', data)
+            .then((response) => {
+                this._source = response;
+                return response;
+            });
+    }
+
+    setSource(source) {
+        this.source = source;
+        return this;
+    }
+
+    getSource(qs) {
+        const path = this._source.resource;
+
+        if (!this._source) return this.getAllSources();
+
+        return this.api(path, 'GET', {}, {}, qs);
+    }
+
+    updateSource(data) {
+        if(!this._source) return Promise.resolve();
+
+        const path = this._source.resource;
+        return this.api(path, 'PUT', data)
+            .then((response) => {
+                this._source = response;
+                return response;
+            });
+    }
+
+    deleteSource(data) {
+        if(!this._source) return Promise.resolve();
+
+        const path = this._source.resource;
+        return this.api(path, 'DELETE', data)
+            .then((response) => {
+                this._source = response;
+                return response;
+            });
+    }
+
+    getAllSources() {
+        const path = this._nextProjectsPage || '/source';
+        const allSources = [];
+        const getAll = () => this.api(path, 'GET')
+              .then((sources) => {
+                  this._nextSourcesPage = sources.meta.next;
+                  allSources.push(sources.objects);
+                  if(sources.meta.next) return getAll();
+                  return allSources;
+              });
+        return getAll();
     }
 };
